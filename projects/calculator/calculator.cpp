0c7883c43033ff603f264a5cc5d7bbd601aa4495
Chris Hyer <hyer.chris@gmail.com>
2023-12-20 09:30:55 -0800

    refactored code so that every action has its own function

--- projects/calculator/calculator.cpp
+++ projects/calculator/calculator.cpp
@@ -4,12 +4,19 @@ using namespace std;
 using namespace std;
 #include <string>
 
+enum Command {
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    UNDEFINED
+};
+
 int add(vector<int> nums) {
     int sum = 0;
     auto it = nums.begin(); // Iterator to the start of the vector
     while (it != nums.end()) {
         sum += *it; // Access the element
-        // cout << *it << " "; Access the element
         ++it; // Move to the next element
     }
     return sum;
@@ -18,7 +25,6 @@ int add(vector<int> nums) {
 bool isNum(char *str) {
     int i = 0;
     while( str[i] != '\0' ) {
-        cout << "char: " << str[i] << "\n";
         if(!isdigit(str[i])) {
             return false;
         }
@@ -28,51 +34,59 @@ bool isNum(char *str) {
     // return isdigit(str[0]) != 0;
 }
 
-vector<int> parseInput(char *str) {
+Command getCommand(char *str) {
+    if(strcmp(str, "add") == 0) {
+        return ADD;
+    } else if(strcmp(str, "sub") == 0) {
+        return SUB;
+    } else if(strcmp(str, "mul") == 0) {
+        return MUL;
+    } else if(strcmp(str, "div") == 0) {
+        return DIV;
+    } else {
+        return UNDEFINED;
+    }
+}
+
+vector<int> parseNums(int size, char** inputArr) {
     vector<int> nums;
-    int i = 0;
-    while( str[i] != '\0' ) {
-        cout << "char: " << str[i] << "\n";
-        if(isdigit(str[i])) {
-            nums.push_back(str[i]);
+    for(int i = 0; i < size; ++i) {
+        if(isNum(inputArr[i])) {
+            nums.push_back(std::stoi(inputArr[i]));
         }
-        i++;
     }
     return nums;
 }
 
+int runCalculator(int argc, char** argv) {
 
-int main(int argc, char** argv) {
-    // cout << "Test main function \n";
-    // int nums[5] = {1,2,3,4,5};
-    // int arrSize = sizeof(nums)/sizeof(int);
-    // cout << "sum: " << add(nums,arrSize) << "\n";
-    // cout << isNum("23") << "\n";
-    // cout << isNum("test not a number") << "\n";
     vector<int> nums; // Vector of integers
-    char *command = argv[1];
-
-    // cout << "Sum: " << add(nums) << endl;
-
-    cout << endl;
+    char *command;
 
     if(argc == 1) {
         cout << "No inputs found, please run the program followed by arguments for the calculator \n";
         return 0;
     }
-    for(int i = 2; i < argc; ++i) {
-        cout << argv[i] << "\n";
-        if(isNum(argv[i])) {
-            cout << "is a number \n";
-            nums.push_back(std::stoi(argv[i]));
-        } else {
-            cout << "is not a number \n";
-        }
+    if(argc == 2) {
+        cout << "No numbers found, please run the program followed by arguments for the calculator \n";
+        return 0;
     }
-    if(strcmp(command, "add") == 0) {
-        cout << "Sum: " << add(nums) << endl;
-    } else {
-        cout << "Command not found \n";
+
+    command = argv[1];
+    nums = parseNums(argc, argv);
+
+    switch(getCommand(command)) {
+        case ADD:
+            cout << "Sum: " << add(nums) << endl;
+            break;
+        default:
+            cout << "Command not found \n";
+            break;
     }
+
     return 0;
+}
+
+int main(int argc, char** argv) {
+    return runCalculator(argc, argv);
 }
